20dc0fc6bb6b693ca5ef73b8ddf67ad2caa38b5d
John Sumskas <johnsumskas@johnsumskas-ThinkPad-P52s>
2021-06-22 16:40:05 +1000

    Update CMakeLists.txt, Packet.msg, and 8 more files...
    Added support for bplprotocol for python 2.7. Created a simple passthrough ros node for bpl stuff

--- bplprotocol/setup.py
+++ bplprotocol/setup.py
@@ -1,9 +1,20 @@
 from distutils.core import setup
+import sys
 
-setup(
-    name='bplprotocol',
-    version='0.1dev',
-    packages=['bplprotocol'],
-    install_requires=['cobs', 'crcmod'],
-    long_description=open('README.md').read(),
-)
+if sys.version_info[0] == 3:
+    setup(
+        name='bplprotocol',
+        version='0.1dev',
+        packages=['bplprotocol'],
+        install_requires=['cobs', 'crcmod'],
+        long_description=open('README.md').read(),
+    )
+else:
+    setup(
+        name='bplprotocol',
+        version='0.2dev',
+        package_dir={"": 'src27'},
+        packages=['bplprotocol'],
+        install_requires=['cobs', 'crcmod'],
+        long_description=open('README.md').read(),
+    )
