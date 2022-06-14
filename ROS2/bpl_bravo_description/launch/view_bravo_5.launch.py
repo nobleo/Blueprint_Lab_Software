9bc39a9cf0b3f7bc40b5aa926bb69b00e6ba8efb
John Sumskas <john@sumskas.com>
2022-06-14 14:50:05 +1000

    Updated to use xacro

--- ROS2/bpl_bravo_description/launch/view_bravo_5.launch.py
+++ ROS2/bpl_bravo_description/launch/view_bravo_5.launch.py
@@ -2,21 +2,20 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.descriptions import ParameterValue
-from launch.substitutions import LaunchConfiguration, Command
 
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     pkg_share = get_package_share_directory('bpl_bravo_description')
-    urdf_file_name = 'urdf/bravo_7_example.urdf.xacro'
+    urdf_file_name = 'urdf/bravo_5_example.urdf.xacro'
     urdf_path = os.path.join(pkg_share, urdf_file_name)
 
     rviz_config_file = os.path.join(pkg_share, 'rviz/rviz.rviz')
-
+    
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
